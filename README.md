de3a2c40db9eda9d676fb949ff46c9cb7b2e778e
Tobias Davis <tobias@davistobias.com>
2023-07-24 07:57:25 -0500

    init: this is basically all it does

--- README.md
+++ README.md
@@ -1,2 +1,49 @@
-# obsidian-gtd-no-next-step
-Obsidian plugin for GTD workflow, badge projects with no next step.
+# GTD: No Next Step
+
+Obsidian plugin for GTD workflow that adds a badge to projects with no defined next step.
+
+## My GTD Workflow
+
+My workflow in Obsidian is that each GTD "project" is a file within a folder named "Projects":
+
+```
+Projects/
+	Get restored files to dad.md
+	Launch awesome podcast.md
+	Replace van windshield.md
+```
+
+I also use the [Tasks](https://publish.obsidian.md/tasks/) plugin, which lets you add tasks like normal, and add views that query them:
+
+```md
+- [ ] call Fred to get the phone number of the windshield place #next-step
+```
+
+This works well for me, because I can set up task queries for `#next-step` tagged tasks:
+
+```md
+\```tasks
+not done
+tags includes #next-step
+\```
+```
+
+I also use the `#waiting-for` tag to indicate things that are a Next Step but rely on an external party:
+
+```md
+- [ ] #waiting-for Fred to get back to me with the windshield guys number
+```
+
+## Motivation for Plugin
+
+What I would like to know is whether there are any "projects" (files in the "Projects" folder) that do not have a Next Step, and are not Waiting For.
+
+These project files would need examination, probably some tidying: maybe the project is complete, or maybe I haven't actually gone through the mental effort of deciding the next step.
+
+## What it Looks Like
+
+![](./example.png)
+
+## License
+
+Published and made available freely under the [Very Open License](http://veryopenlicense.com/).
---
+++ main.js
@@ -0,0 +1,138 @@
+const { Notice, Plugin, PluginSettingTab, Setting } = require('obsidian')
+
+const DEFAULT_SETTINGS = {
+	nextStepTag: 'next-step',
+	waitingForTag: 'waiting-for',
+	projectsFolderPrefix: 'Projects',
+	projectFileCache: {
+		// ['Projects/example.md']: { mtime: 123, hasNextStep: true }
+	},
+}
+
+const makeTaskRegex = tagString => new RegExp(`^\\s*-\\s{1,2}\\[\\s]\\s.*#${tagString}[\\W]*`, 'm')
+
+module.exports = class MyPlugin extends Plugin {
+	async onload() {
+		await this.loadSettings()
+		this.nextStepTagRegex = makeTaskRegex(this.settings.nextStepTag)
+		this.waitingForTagRegex = makeTaskRegex(this.settings.waitingForTag)
+
+		this.registerEvent(this.app.vault.on('delete', async () => { await this.refreshBadges() }))
+		this.registerEvent(this.app.vault.on('rename', async () => { await this.refreshBadges() }))
+		this.registerEvent(this.app.vault.on('modify', async () => { await this.refreshBadges() }))
+
+		this.app.workspace.onLayoutReady(this.initialize)
+		this.addSettingTab(new SettingTab(this.app, this))
+	}
+
+	async loadSettings() {
+		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData())
+	}
+
+	async saveSettings() {
+		await this.saveData(this.settings || DEFAULT_SETTINGS)
+	}
+
+	isProjectFile = filename => filename.startsWith(this.settings.projectsFolderPrefix)
+		&& filename.endsWith('.md')
+		&& !filename.includes('/_')
+
+	containsIncompleteNextStep = string => this.nextStepTagRegex.test(string)
+	containsIncompleteWaitingFor = string => this.waitingForTagRegex.test(string)
+
+	refreshBadges = async () => {
+		const projectFilesList = this
+			.app
+			.vault
+			.getMarkdownFiles()
+			.filter(f => this.isProjectFile(f.path))
+		const filesMap = {}
+		let needToSave = false
+		for (const file of projectFilesList) {
+			filesMap[file.path] = this.settings.projectFileCache[file.path] || {
+				mtime: file.stat.mtime,
+			}
+			const lastCache = this.settings.projectFileCache[file.path]
+			if (file.stat.mtime > (lastCache ? lastCache.mtime : 0)) {
+				needToSave = true
+				const string = await this.app.vault.read(file)
+				filesMap[file.path].nextStep = this.containsIncompleteNextStep(string)
+				filesMap[file.path].waitingFor = this.containsIncompleteWaitingFor(string)
+				console.log('--', file.path, filesMap[file.path])
+			}
+		}
+		for (const path in this.settings.projectFileCache)
+			if (!filesMap[path]) needToSave = true
+		if (needToSave) {
+			this.settings.projectFileCache = filesMap
+			await this.saveSettings()
+		}
+		// update badge class
+		const leaves = this.app.workspace.getLeavesOfType('file-explorer')
+		if (!leaves.length) console.error('Tobias says: I saw in one plugin that this might not load right away, so they added retry logic with a setTimeout')
+		else {
+			const fileItems = leaves[0].view.fileItems
+			for (const f in fileItems) if (this.isProjectFile(f)) {
+				const cached = filesMap[f]
+				if (cached && !cached.nextStep && !cached.waitingFor) {
+					fileItems[f].coverEl.addClass('gtd-no-next-step')
+				} else if (cached && cached.waitingFor) {
+					fileItems[f].coverEl.removeClass('gtd-no-next-step')
+					fileItems[f].coverEl.addClass('gtd-waiting-for')
+				} else {
+					fileItems[f].coverEl.removeClass('gtd-no-next-step')
+					fileItems[f].coverEl.removeClass('gtd-waiting-for')
+				}
+			}
+		}
+	}
+
+	initialize = () => { this.refreshBadges() }
+}
+
+class SettingTab extends PluginSettingTab {
+	constructor(app, plugin) {
+		super(app, plugin)
+		this.plugin = plugin
+	}
+	display() {
+		const { containerEl } = this
+		containerEl.empty()
+		new Setting(containerEl)
+			.setName('Projects Folder')
+			.setDesc('The prefix for project files, e.g. for a folder use "Projects/".')
+			.addText(
+				text => text
+					.setPlaceholder('Projects/')
+					.setValue(this.plugin.settings.projectsFolderPrefix)
+					.onChange(async (value) => {
+						this.plugin.settings.projectsFolderPrefix = value
+						await this.plugin.saveSettings()
+					})
+			)
+		new Setting(containerEl)
+			.setName('Next-Step Tag')
+			.setDesc('The tag (without #) that indicates a task has a next step.')
+			.addText(
+				text => text
+					.setPlaceholder('next-step')
+					.setValue(this.plugin.settings.nextStepTag)
+					.onChange(async (value) => {
+						this.plugin.settings.nextStepTag = value
+						await this.plugin.saveSettings()
+					})
+			)
+		new Setting(containerEl)
+			.setName('Waiting-For Tag')
+			.setDesc('The tag (without #) that indicates a task is waiting for an external action.')
+			.addText(
+				text => text
+					.setPlaceholder('waiting-for')
+					.setValue(this.plugin.settings.waitingForTag)
+					.onChange(async (value) => {
+						this.plugin.settings.waitingForTag = value
+						await this.plugin.saveSettings()
+					})
+			)
+	}
+}
