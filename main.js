3e6b6bc08ca34d448823ebc920f3435b56fa3af7
Tobias Davis <tobias@davistobias.com>
2023-08-12 21:36:40 -0500

    fix: use cached read, tfile, and tidy logic

--- main.js
+++ main.js
@@ -1,25 +1,39 @@
 const { Notice, Plugin, PluginSettingTab, Setting } = require('obsidian')
 
 const DEFAULT_SETTINGS = {
-	nextStepTag: 'next-step',
-	waitingForTag: 'waiting-for',
-	projectsFolderPrefix: 'Projects',
+	nextStepTag: '#next-step',
+	waitingForTag: '#waiting-for',
+	projectsFolderPrefix: 'Projects/',
 	projectFileCache: {
-		// ['Projects/example.md']: { mtime: 123, hasNextStep: true }
+		// ['Projects/example.md']: { mtime: 123, nextStep: true, waitingFor: true }
 	},
 }
 
-const makeTaskRegex = tagString => new RegExp(`^\\s*-\\s{1,2}\\[\\s]\\s.*#${tagString}[\\W]*`, 'm')
+const makeTaskRegex = tagString => new RegExp(`^\\s*-\\s{1,2}\\[\\s]\\s.*${tagString}[\\W]*`, 'm')
 
-module.exports = class GtdNoNextStep extends Plugin {
+const updateFileBadges = (file, fileItem, isProjectFile) => {
+	const { nextStep, waitingFor } = file || {}
+	if (isProjectFile && !nextStep && !waitingFor) {
+		fileItem.coverEl.addClass('gtd-no-next-step')
+	} else if (isProjectFile && file.waitingFor) {
+		fileItem.coverEl.removeClass('gtd-no-next-step')
+		fileItem.coverEl.addClass('gtd-waiting-for')
+	} else {
+		fileItem.coverEl.removeClass('gtd-no-next-step')
+		fileItem.coverEl.removeClass('gtd-waiting-for')
+	}
+}
+
+module.exports = class MyPlugin extends Plugin {
 	async onload() {
 		await this.loadSettings()
+		// TODO make lazy
 		this.nextStepTagRegex = makeTaskRegex(this.settings.nextStepTag)
 		this.waitingForTagRegex = makeTaskRegex(this.settings.waitingForTag)
 
-		this.registerEvent(this.app.vault.on('delete', async () => { await this.refreshBadges() }))
-		this.registerEvent(this.app.vault.on('rename', async () => { await this.refreshBadges() }))
-		this.registerEvent(this.app.vault.on('modify', async () => { await this.refreshBadges() }))
+		this.registerEvent(this.app.vault.on('delete', async event => { await this.refreshFileBadges(event) }))
+		this.registerEvent(this.app.vault.on('rename', async event => { await this.refreshFileBadges(event) }))
+		this.registerEvent(this.app.vault.on('modify', async event => { await this.refreshFileBadges(event) }))
 
 		this.app.workspace.onLayoutReady(this.initialize)
 		this.addSettingTab(new SettingTab(this.app, this))
@@ -40,7 +54,37 @@ module.exports = class GtdNoNextStep extends Plugin {
 	containsIncompleteNextStep = string => this.nextStepTagRegex.test(string)
 	containsIncompleteWaitingFor = string => this.waitingForTagRegex.test(string)
 
-	refreshBadges = async () => {
+	refreshFileBadges = async ({ path, stat, deleted, ...remaining }) => {
+		const cached = this.settings.projectFileCache[path] || {}
+		const pathIsProjectFile = this.isProjectFile(path)
+		let needToSave
+		let removeBadges
+		if (!deleted && pathIsProjectFile) {
+			const string = await this.app.vault.cachedRead(
+				this.app.vault.getAbstractFileByPath(path)
+			)
+			const nextStep = this.containsIncompleteNextStep(string)
+			if (cached?.nextStep !== nextStep) cached.nextStep = nextStep
+			const waitingFor = this.containsIncompleteWaitingFor(string)
+			if (cached?.waitingFor !== waitingFor) cached.waitingFor = waitingFor
+			cached.mtime = stat.mtime
+			needToSave = true
+		} else if (this.settings.projectFileCache[path]) {
+			delete this.settings.projectFileCache[path]
+			needToSave = true
+		}
+		if (needToSave) {
+			this.settings.projectFileCache[path] = cached
+			await this.saveSettings()
+		}
+		window.setTimeout(() => {
+			const leaves = this.app.workspace.getLeavesOfType('file-explorer')
+			if (leaves?.[0]?.view?.fileItems?.[path])
+				updateFileBadges(cached, leaves[0].view.fileItems[path], pathIsProjectFile)
+		})
+	}
+
+	refreshProjectBadges = async () => {
 		const projectFilesList = this
 			.app
 			.vault
@@ -48,17 +92,16 @@ module.exports = class GtdNoNextStep extends Plugin {
 			.filter(f => this.isProjectFile(f.path))
 		const filesMap = {}
 		let needToSave = false
-		for (const file of projectFilesList) {
-			filesMap[file.path] = this.settings.projectFileCache[file.path] || {
-				mtime: file.stat.mtime,
+		for (const tFile of projectFilesList) {
+			filesMap[tFile.path] = this.settings.projectFileCache[tFile.path] || {
+				mtime: tFile.stat.mtime,
 			}
-			const lastCache = this.settings.projectFileCache[file.path]
-			if (file.stat.mtime > (lastCache ? lastCache.mtime : 0)) {
+			const lastCache = this.settings.projectFileCache[tFile.path]
+			if (tFile.stat.mtime > (lastCache ? lastCache.mtime : 0)) {
 				needToSave = true
-				const string = await this.app.vault.read(file)
-				filesMap[file.path].nextStep = this.containsIncompleteNextStep(string)
-				filesMap[file.path].waitingFor = this.containsIncompleteWaitingFor(string)
-				console.log('--', file.path, filesMap[file.path])
+				const string = await this.app.vault.cachedRead(tFile)
+				filesMap[tFile.path].nextStep = this.containsIncompleteNextStep(string)
+				filesMap[tFile.path].waitingFor = this.containsIncompleteWaitingFor(string)
 			}
 		}
 		for (const path in this.settings.projectFileCache)
@@ -67,27 +110,20 @@ module.exports = class GtdNoNextStep extends Plugin {
 			this.settings.projectFileCache = filesMap
 			await this.saveSettings()
 		}
-		// update badge class
 		const leaves = this.app.workspace.getLeavesOfType('file-explorer')
-		if (!leaves.length) console.error('Tobias says: I saw in one plugin that this might not load right away, so they added retry logic with a setTimeout')
-		else {
-			const fileItems = leaves[0].view.fileItems
+		if (leaves?.length) {
+			const fileItems = leaves[0].view?.fileItems || {}
 			for (const f in fileItems) if (this.isProjectFile(f)) {
-				const cached = filesMap[f]
-				if (cached && !cached.nextStep && !cached.waitingFor) {
-					fileItems[f].coverEl.addClass('gtd-no-next-step')
-				} else if (cached && cached.waitingFor) {
-					fileItems[f].coverEl.removeClass('gtd-no-next-step')
-					fileItems[f].coverEl.addClass('gtd-waiting-for')
-				} else {
-					fileItems[f].coverEl.removeClass('gtd-no-next-step')
-					fileItems[f].coverEl.removeClass('gtd-waiting-for')
-				}
+				updateFileBadges(filesMap[f], fileItems[f], true)
 			}
 		}
 	}
 
-	initialize = () => { this.refreshBadges() }
+	initialize = () => {
+		this.refreshProjectBadges().catch(error => {
+			console.error('Unexpected error in "gtd-no-next-step" plugin initialization.', error)
+		})
+	}
 }
 
 class SettingTab extends PluginSettingTab {
@@ -99,11 +135,11 @@ class SettingTab extends PluginSettingTab {
 		const { containerEl } = this
 		containerEl.empty()
 		new Setting(containerEl)
-			.setName('Projects Folder')
-			.setDesc('The prefix for project files, e.g. for a folder use "Projects/".')
+			.setName('Projects folder')
+			.setDesc('The folder where project files live, e.g. "Projects/".')
 			.addText(
 				text => text
-					.setPlaceholder('Projects/')
+					.setPlaceholder(DEFAULT_SETTINGS.projectsFolderPrefix)
 					.setValue(this.plugin.settings.projectsFolderPrefix)
 					.onChange(async (value) => {
 						this.plugin.settings.projectsFolderPrefix = value
@@ -111,11 +147,11 @@ class SettingTab extends PluginSettingTab {
 					})
 			)
 		new Setting(containerEl)
-			.setName('Next-Step Tag')
-			.setDesc('The tag (without #) that indicates a task has a next step.')
+			.setName('Next-Step tag')
+			.setDesc('The tag that indicates a task has a next step.')
 			.addText(
 				text => text
-					.setPlaceholder('next-step')
+					.setPlaceholder(DEFAULT_SETTINGS.nextStepTag)
 					.setValue(this.plugin.settings.nextStepTag)
 					.onChange(async (value) => {
 						this.plugin.settings.nextStepTag = value
@@ -123,11 +159,11 @@ class SettingTab extends PluginSettingTab {
 					})
 			)
 		new Setting(containerEl)
-			.setName('Waiting-For Tag')
-			.setDesc('The tag (without #) that indicates a task is waiting for an external action.')
+			.setName('Waiting-For tag')
+			.setDesc('The tag that indicates a task is waiting for an external action.')
 			.addText(
 				text => text
-					.setPlaceholder('waiting-for')
+					.setPlaceholder(DEFAULT_SETTINGS.waitingForTag)
 					.setValue(this.plugin.settings.waitingForTag)
 					.onChange(async (value) => {
 						this.plugin.settings.waitingForTag = value
